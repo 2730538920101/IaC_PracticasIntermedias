a173bf3167306c86a9ff7489e45505cb1a978d12
carlosmz87 <carlosmartz1995@gmail.com>
2023-07-29 09:07:32 -0600

    [ADD] Infrastructura inicial del proyecto

---
+++ ec2.tf
@@ -0,0 +1,95 @@
+resource "aws_key_pair" "PracticasI-key" {
+  key_name   = "PracticasI"
+  public_key = file("${var.PATH_PUBLIC_KEYPAIR}")
+}
+
+resource "aws_instance" "ec2_bastion" {
+  ami                    = "ami-053b0d53c279acc90"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.PracticasI-key.key_name
+  vpc_security_group_ids = [aws_security_group.PracticasI-sg.id]
+  subnet_id              = aws_subnet.PublicS1.id
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y",
+      "sudo apt upgrade -y",
+      "sudo apt install mysql-server -y",
+      "sudo apt install python3",
+      "sudo apt install git",
+      "sudo apt install nginx"
+    ]
+  }
+  connection {
+    host        = self.public_ip
+    user        = var.user_ssh
+    private_key = file("${var.PATH_KEYPAIR}")
+  }
+  depends_on = [
+    aws_db_instance.mysql-instance
+  ]
+}
+
+resource "aws_security_group" "PracticasI-sg" {
+  vpc_id = aws_vpc.PracticasI.id
+  name   = "PracticasI-sg"
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 3306
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 3306
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 4200
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 4200
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 5000
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 5000
+    }
+  ]
+}
+
